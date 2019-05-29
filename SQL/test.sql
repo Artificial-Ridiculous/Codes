9bdaa69b0b688d74c8009831dbc80c7da04389d1
cocobolo <happylinzhe@gmail.com>
2019-05-29 23:17:01 +0800

    update

--- SQL/test.sql
+++ SQL/test.sql
@@ -1,15 +1,6 @@
 use test;
-select if (
-(select count(*) from a) = 1
+select if((select count( distinct id) from a) >2 
 ,
-(select num
-from my_numbers
-group by num
-having count(num)=1
-order by num desc
-limit 1) as a
+select 8
 ,
-null
-) 
-
-as num
+select 9)
