57fddf9ff5b502cbb34e4835ab558967c7f935e1
Artificial-Ridiculous <happylinzhe@gmail.com>
2019-05-30 22:28:10 +0800

    commit

--- Python/leetconde-cn/test3.py
+++ Python/leetconde-cn/test3.py
@@ -1,15 +1,19 @@
 class Solution:
-    def minIncrementForUnique(self, A: List[int]) -> int:
-        B = sorted(A)
+    def minIncrementForUnique(self, A):
+        A.sort()
         cnt = 0
-        for i in range(1,len(B)):
-            while (B[i] <= B[i-1]):
-                cnt+=1
+        B = [A[0]]
+        for i in range(1,len(A)):
+            if A[i] <= B[-1]:
+                cnt += B[-1]+1-A[i]
+                B.append(B[-1]+1)
+            else: B.append(A[i])
         return cnt
 
 if __name__ == '__main__':
-    '''solution = Solution()
+    solution = Solution()
+    #A = [3,2,1,2,1,7]
     A = [1,2,2]
     ans = solution.minIncrementForUnique(A)
-    print(ans)'''
-    print(0)
+    print(ans)
+    
