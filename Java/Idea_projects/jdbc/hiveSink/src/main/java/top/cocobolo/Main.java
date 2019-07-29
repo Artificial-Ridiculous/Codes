773ea7b75a254d35bde5437d69b58b929fdafcec
cocobolo <happylinzhe@gmail.com>
2019-07-29 17:38:50 +0800

    把单sink正确的代码改成多sink错误的半成品  20190729-17：38下班

--- Java/Idea_projects/jdbc/hiveSink/src/main/java/top/cocobolo/Main.java
+++ Java/Idea_projects/jdbc/hiveSink/src/main/java/top/cocobolo/Main.java
@@ -35,7 +35,7 @@ public class Main {
         )
             .setParallelism(1)
             .map(string -> JSON.parseObject(string, Transaction.class))
-            .addSink(new HiveSinkBatch(countThreshold, interval)).setParallelism(1); //数据 sink 到 mysql
+            .addSink(new HiveSinkBatch(countThreshold, interval)).setParallelism(2); //数据 sink 到 mysql
 
         env.execute("Flink add sink");
     }
